7c6832ff1090b2cc455a57263f65165012572140
bvcalipes <bvcalipes@brs.pshs.edu.ph>
2026-08-20 13:23:40 +0800

    DMM

---
+++ activity.py
@@ -0,0 +1,12 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print(f"{self.name} Roooof!")
+
+
+d1 = Dog("Chopper", 1)
+
+d1.bar
